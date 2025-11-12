7821e77981812d36508ebcf8fa89b31de65e0e02
Bruno Rocha <rochacbruno@gmail.com>
2025-11-12 17:49:15 +0000

    :recycle: apps folder

--- src/framework/cli.py
+++ src/framework/cli.py
@@ -16,19 +16,19 @@ app = App(
 REPO = os.getenv("FRAMEWORK_REPO", "https://github.com/rochacbruno/framework")
 
 
-class Context(BaseModel):
-    project_template: str = REPO
-    app_template: str | Path
+# class Context(BaseModel):
+#     project_template: str = REPO
+#     app_template: str | Path
 
-    @classmethod
-    def build(cls):
-        # This local clone is used only once to bootstrap each app
-        temp_dir = TemporaryDirectory(delete=False)
-        base_cache_path = Path(temp_dir.name)
-        Repo.clone_from(REPO, base_cache_path)
-        print(f"Cloned {REPO} to {base_cache_path}")
-        app_template = base_cache_path / "templates/app"
-        return cls(app_template=app_template)
+#     @classmethod
+#     def build(cls):
+#         # This local clone is used only once to bootstrap each app
+#         temp_dir = TemporaryDirectory(delete=False)
+#         base_cache_path = Path(temp_dir.name)
+#         Repo.clone_from(REPO, base_cache_path)
+#         print(f"Cloned {REPO} to {base_cache_path}")
+#         app_template = base_cache_path / "templates/app"
+#         return cls(app_template=app_template)
 
 
 @app.command
@@ -51,10 +51,10 @@ def init(
     if not Path(destination / ".git"):
         Repo.init(str(destination))
 
-    ctx = Context.build()
+    # ctx = Context.build()
     print(f"Initializing your project on {destination}")
     run_copy(
-        ctx.project_template,
+        REPO,
         destination,
         data={
             "project_name": project,
@@ -63,8 +63,16 @@ def init(
     )
     print("Main project created.")
 
+    apps_destination = destination / "apps"
     for app_name in apps:
-        run_copy(str(ctx.app_template), destination / app_name)
+        # run_copy(str(ctx.app_template), destination / app_name)
+        run_copy(
+            REPO,
+            apps_destination / app_name,
+            data={
+                "template": "templates/app",
+            },
+        )
         print(f"Created app {app_name}")
 
 
