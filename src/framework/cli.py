0a1da0278714f953b5b905289c597ae8a4e457a0
Bruno Rocha <rochacbruno@gmail.com>
2025-11-12 12:10:13 +0000

    :recycle: project as arg

--- src/framework/cli.py
+++ src/framework/cli.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from copier import run_copy, run_update
 from cyclopts import App
@@ -10,39 +12,32 @@ app = App(
     help="Framework for building Django applications",
 )
 
+REPO = os.getenv("FRAMEWORK_REPO", "https://github.com/rochacbruno/framework")
+
 
 class Context(BaseModel):
-    project_template: Path = Path()
-    app_template: Path = Path()
+    project_template: str
+    app_template: str | Path
 
     @classmethod
     def build(cls):
-        app_template = None
-        project_template = None
-
-        if Path("copier.yaml").exists():
-            project_template = Path.cwd()
-        if Path("templates/app").exists():
-            app_template = Path("templates/app")
-
-        if app_template and project_template:
-            return cls(
-                project_template=Path("templates/project"),
-                app_template=Path("templates/app"),
-            )
-
-        base_cache_path = Path("/tmp/framework_cache")
-        if not base_cache_path.exists():
-            Repo.clone_from(
-                "https://github.com/rochacbruno/framework.git", base_cache_path
-            )
-        project_template = base_cache_path
+        # This local clone is used only once to bootstrap each app
+        temp_dir = TemporaryDirectory()
+        base_cache_path = Path(temp_dir.name)
+        Repo.clone_from(REPO, base_cache_path)
+        print(f"Cloned {REPO} to {base_cache_path}")
         app_template = base_cache_path / "templates/app"
-        return cls(project_template=project_template, app_template=app_template)
+
+        # Main project is always templated directly from repo
+        return cls(project_template=REPO, app_template=app_template)
 
 
 @app.command
-def init(destination: Path | None = None, apps: list[str] = ["app"]):
+def init(
+    destination: Path | None = None,
+    project: str = "project",
+    apps: list[str] = ["app"],
+):
     """Initialize a new application.
 
     1. Create new project from project_template
@@ -56,12 +51,12 @@ def init(destination: Path | None = None, apps: list[str] = ["app"]):
 
     ctx = Context.build()
     print(f"Initializing your project on {destination}")
-    run_copy(str(ctx.project_template), destination)
+    run_copy(ctx.project_template, destination, data={"project": project})
     print("Main project created.")
 
-    for app_name in apps:
-        run_copy(str(ctx.app_template), destination / app_name)
-        print(f"Created app {app_name}")
+    # for app_name in apps:
+    #     run_copy(str(ctx.app_template), destination / app_name)
+    #     print(f"Created app {app_name}")
 
 
 @app.command
