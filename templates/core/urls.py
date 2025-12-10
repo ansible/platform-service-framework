392c5260a80625349b3f8ca269d60f896560dba8
Hao Liu <44379968+TheRealHaoLiu@users.noreply.github.com>
2025-12-10 01:15:41 -0500

    fix: update core app template for APIRootView integration
    - Remove IndexView from core/urls.py (handled by middleware now)
    - Add apps.core to project_applications by default

--- templates/core/urls.py
+++ templates/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import HealthView, IndexView, PingView
+from .views import HealthView, PingView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
     path("ping/", PingView.as_view(), name="ping"),
     path("health/", HealthView.as_view(), name="health"),
 ]
